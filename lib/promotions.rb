9d9cbcff29751d50446a6f92e1bf46d7d0e07b7a
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-25 14:16:03 +0100

    Add basic Promotions#apply_promotion method

--- lib/promotions.rb
+++ lib/promotions.rb
@@ -1,5 +1,15 @@
 class Promotions
 
+  def apply_promotion(total_before_promotions, basket)
+    @basket = basket
+    @working_total = total_before_promotions
+
+    two_or_more_travel_card_holders
+    over_sixty_pounds
+
+    @working_total.round(2)
+  end
+
   def two_or_more_travel_card_holders
     amount = 0
     @basket.each do | item_code |
@@ -7,7 +17,6 @@ class Promotions
     end
 
     @working_total -= amount * 0.75 if amount >= 2
-
   end
 
   def over_sixty_pounds
--- spec/unit/promotions_spec.rb
+++ spec/unit/promotions_spec.rb
@@ -10,7 +10,7 @@ describe Promotions do
       promotions.instance_variable_set(:@basket, [001, 001])
       promotions.instance_variable_set(:@working_total, 18.50)
 
-      expect { promotions.two_or_more_travel_card_holders }.to change { promotions.instance_variable_get(:@working_total) }.to(17)
+      expect { promotions.two_or_more_travel_card_holders }.to change { promotions.instance_variable_get(:@working_total) }.to(17.00)
     end
   end
 
@@ -19,6 +19,12 @@ describe Promotions do
       promotions.instance_variable_set(:@working_total, 61.00)
 
       expect { promotions.over_sixty_pounds }.to change { promotions.instance_variable_get(:@working_total) }.to(54.90)
+    end
+  end
+
+  describe "#apply_promotion" do
+    it "return a total of the correct promotions" do
+      expect(promotions.apply_promotion(63.50, [001, 001, 002])).to eq(55.80)
     end
   end
 
