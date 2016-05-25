044767f42ef730c69471ef58b3a895607a26951d
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-25 13:54:49 +0100

    Add basic Promotions#over_sixty_pounds method

--- lib/promotions.rb
+++ lib/promotions.rb
@@ -1,6 +1,6 @@
 class Promotions
 
-  def two_or_more_travel_card_holders?
+  def two_or_more_travel_card_holders
     amount = 0
     @basket.each do | item_code |
       amount += 1 if item_code == 001
@@ -9,7 +9,11 @@ class Promotions
     @working_total -= amount * 0.75 if amount >= 2
 
   end
-  
+
+  def over_sixty_pounds
+     @working_total *= 0.9 if @working_total > 60
+  end
+
   private
 
   attr_reader :working_total, :basket
--- spec/unit/promotions_spec.rb
+++ spec/unit/promotions_spec.rb
@@ -10,7 +10,15 @@ describe Promotions do
       promotions.instance_variable_set(:@basket, [001, 001])
       promotions.instance_variable_set(:@working_total, 18.50)
 
-      expect { promotions.two_or_more_travel_card_holders? }.to change { promotions.instance_variable_get(:@working_total) }.to(17)
+      expect { promotions.two_or_more_travel_card_holders }.to change { promotions.instance_variable_get(:@working_total) }.to(17)
+    end
+  end
+
+  describe "#over_sixty_pounds?" do
+    it "should subtract ten percent from the working total when it is over sixty pounds" do
+      promotions.instance_variable_set(:@working_total, 61.00)
+
+      expect { promotions.over_sixty_pounds }.to change { promotions.instance_variable_get(:@working_total) }.to(54.90)
     end
   end
 
