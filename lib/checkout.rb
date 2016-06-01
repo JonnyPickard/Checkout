8f735b4a7bafc1b7dff600a76986f1cba34958b9
Jonny Pickard <jonathandpickard@gmail.com>
2016-06-01 16:33:40 +0100

    Further refactoring 2

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -18,9 +18,7 @@ class Checkout
   end
 
   def total
-    return total_before_promotions if @promotions_calculator.nil?
-
-    @promotions_calculator.apply_promotion(total_before_promotions, @basket)
+    @promotions_calculator.nil? ? total_before_promotions : @promotions_calculator.apply_promotion(total_before_promotions, @basket)
   end
 
   private
--- lib/products/add_products.rb
+++
@@ -1,9 +0,0 @@
-module AddProducts
-  def initialize(product_list=ProductList::items)
-    @product_list = product_list
-  end
-
-  def self.new_products(item)
-    @product_list[item[:item_code]][[item[:product], item[:price]]]
-  end
-end
